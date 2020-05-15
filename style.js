25a9d416e0cd4faaa740a9c873c88329ba1c3a42
swimmy <sugarbluexx@gmail.com>
2020-05-16 01:00:08 +0900

    style6 css fix

--- style.js
+++ style.js
@@ -1,22 +1,22 @@
 function handleSubmit() {
-  var messageInput = document.querySelector(".msg");
-  if (messageInput.value === "") return;
-  var messageList = document.querySelector(".container__talk");
-  var baseNode = document.querySelector(".talk__item-right");
-  var pendingMessage = document.querySelectorAll(".talk__item-left")[1];
+  var messageInput = document.querySelector('.msg');
+  if (messageInput.value === '') return;
+  var messageList = document.querySelector('.container__talk');
+  var baseNode = document.querySelector('.talk__item-right');
+  var pendingMessage = document.querySelectorAll('.talk__item-left')[1];
   var messageList = pendingMessage.parentNode;
   var messageItem = baseNode.cloneNode(true);
-  messageItem.querySelector(".item_talk-text").innerText = messageInput.value;
-  messageItem.querySelector(".item_talk-time-right").innerText =
-    "in a few seconds ago";
+  messageItem.querySelector('.item_talk-text').innerText = messageInput.value;
+  messageItem.querySelector('.item_talk-time-right').innerText =
+    'in a few seconds ago';
   //var article = document.createElement("article");
   //article.classList.add("list__item-r");
   //article.innerText = messageInput.value;
   messageList.insertBefore(messageItem, pendingMessage);
-  messageInput.value = "";
+  messageInput.value = '';
 }
 
-window.addEventListener("DOMContentLoaded", function () {
-  var submitButton = document.querySelector(".btn-send");
-  submitButton.addEventListener("click", handleSubmit);
+window.addEventListener('DOMContentLoaded', function () {
+  var submitButton = document.querySelector('.send-btn');
+  submitButton.addEventListener('click', handleSubmit);
 });
